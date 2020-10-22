0df945ebbbf5da75bf78e591d03a6c19e9edcdcf
nkekre <59002184+nkekre@users.noreply.github.com>
2020-10-21 23:46:40 -0400

    Initial Commit

---
+++ MovieAppIT315/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieAppIT315
+//
+//  Created by Namita Kekre on 10/21/20.
+//  Copyright © 2020 nkekre Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
